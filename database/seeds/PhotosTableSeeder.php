25761daa5d6b6a839bff3ff59569c474ccd651dc
Luca Spennato <luca.spennato@gmail.com>
2022-10-11 16:58:02 +0200

    feat: :wrench: seeders creation

---
+++ database/seeds/PhotosTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PhotosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}

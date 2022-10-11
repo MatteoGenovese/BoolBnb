ec05f6aff4dc36f1aef1abd2bf9a43410ae1238c
MatteoGenovese <matteo.genovese@icloud.com>
2022-10-11 12:38:48 +0200

    feat: :sparkles: created apartments table

---
+++ database/migrations/2022_10_11_101628_create_apartments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 100);
+            $table->text('description', 10000);
+            $table->tinyInteger('bathroom_no')->unsigned();
+            $table->tinyInteger('bed_no')->unsigned();
+            $table->tinyInteger('room_no')->unsigned();
+            $table->smallInteger('square_meters')->unsigned();
+            $table->string('address', 100);
+            $table->float('latitude', 9, 6);
+            $table->float('longitude', 9, 6);
+            $table->boolean('is_available')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}

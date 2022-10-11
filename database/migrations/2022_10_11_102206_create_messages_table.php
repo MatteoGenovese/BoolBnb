ce984ea0f52e1162a458e0c3c490c80fb0b9cd12
MatteoGenovese <matteo.genovese@icloud.com>
2022-10-11 12:39:08 +0200

    feat: :sparkles: created messages table

---
+++ database/migrations/2022_10_11_102206_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('username', 50)->nullable();
+            $table->string('email', 50);
+            $table->text('message', 1000);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}

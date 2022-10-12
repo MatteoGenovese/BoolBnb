eed6fffed381a994b875a90d836476559d73729a
Luca Spennato <luca.spennato@gmail.com>
2022-10-12 16:15:19 +0200

    feat: :art: navbar creation, npm i bootstrap 5.2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
 </head>
 <body>
     <div id="app">
+        
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -52,6 +53,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item mx-5">
+                                <a href="{{ route('user.apartments.create') }}" class="btn btn-success m-auto">Crea</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}

a5667d58e3f858425061ae412e88efad043b42e2
Rossiitaa <rosario.scuotto@outlook.it>
2022-10-13 19:42:12 +0200

    feat: :art: single card with more info

--- resources/views/user/apartments/show.blade.php
+++ resources/views/user/apartments/show.blade.php
@@ -1,17 +1,37 @@
-// TODO: Aggiungere statistiche e sponsorship
+{{-- // TODO: Aggiungere statistiche e sponsorship --}}
 @extends("layouts.app")
 
 @section("content")
-
-    <div class="card" style="width: 18rem;">
-        @forelse ($apartment->photos as $photo)
-            <img src="{{ asset('storage/'. $photo->file_name) }}" class="card-img-top" alt="FOTONA">
-            @empty
-            No photo
-        @endforelse
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-column text-center">
+                <div class="card">
+                    @forelse ($apartment->photos as $photo)
+                        <img src="{{ asset('storage/'. $photo->file_name) }}" class="w-50 align-self-center mt-5" alt="FOTONA">
+                        @empty
+                        No photo
+                    @endforelse
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $apartment->title }}</h4>
+                        <p class="card-text">{{ $apartment->description }}</p>
+                        <p class="card-text">{{ $apartment->bathroom_no }}</p>
+                        <p class="card-text">{{ $apartment->bed_no }}</p>
+                        <p class="card-text">{{ $apartment->bathroom_no }}</p>
+                        <p class="card-text">{{ $apartment->room_no }}</p>
+                        <p class="card-text">{{ $apartment->square_meters }}</p>
+                        <p class="card-text">{{ $apartment->address }}</p>
+                        <form action="{{ route('user.apartments.destroy', $apartment->id) }}" method="POST" >
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-warning">
+                                Cestinalo
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-
+    </div>
+    
+    
 @endsection

3597fde6473c20d7bc2a61d97166ac942c27f92b
Rossiitaa <rosario.scuotto@outlook.it>
2022-10-13 19:02:19 +0200

    fix: :fire: minor typos in code

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -11,7 +11,7 @@
                         <th scope="col">Titolo</th>
                         <th scope="col">Indirizzo</th>
                         <th scope="col">Messaggi</th>
-                        <th scope="col"><div class="text-center">Azione</div></th>
+                        <th scope="col"><div class="text-center">Azione</th>
                     </tr>
                 </thead>
 
@@ -21,7 +21,7 @@
                             <th scope="row">{{ $apartment->id }}</th>
                             <td>{{ $apartment->title }}</td>
                             <td>{{ $apartment->address }}</td>
-                            <td class="text-center">{{ $apartment->messages->count() }}</td>
+                            <td>{{ $apartment->messages->count() }}</td>
                             <td class="text-center">
                                 <a class="btn btn-primary m-1" href="{{ route('user.apartments.show', $apartment->id) }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
@@ -51,10 +51,6 @@
                                 <a class="btn btn-warning m-1" href="
                                 {{ route('user.apartments.assignSponsorship', $apartment->id )}}
                                 " >
-
-
-
-
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                     </svg>

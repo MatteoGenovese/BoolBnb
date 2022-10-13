4d4438726f575a84de096f7f499beabd635ef553
DanCan30 <danielecan3008@gmail.com>
2022-10-13 17:24:55 +0200

    Minor language change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">Registrati</a>
                                 </li>
                             @endif
                         @else

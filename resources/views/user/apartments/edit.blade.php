4c4f4cf463b52351146fa75ecf2bbfbcbce7ac49
MatteoGenovese <matteo.genovese@icloud.com>
2022-10-17 15:40:42 +0200

    feat: autocomple->off

--- resources/views/user/apartments/edit.blade.php
+++ resources/views/user/apartments/edit.blade.php
@@ -8,7 +8,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <form action="{{ route('user.apartments.update', $apartment->id ) }}" method="post" class="crud-form" enctype="multipart/form-data">
+                    <form action="{{ route('user.apartments.update', $apartment->id ) }}" method="post" autocomplete="off" class="crud-form" enctype="multipart/form-data">
                         @csrf
                         @method('put')
                         @include('user.apartments.includes.form')

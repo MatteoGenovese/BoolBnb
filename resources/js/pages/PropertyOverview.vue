3175b2ef7d42ce8af5ed5b8d7d80437f01ab96f2
Luca Spennato <luca.spennato@gmail.com>
2022-10-21 17:40:01 +0200

    feat: :art: creating a dinamic navbar

--- resources/js/pages/PropertyOverview.vue
+++ resources/js/pages/PropertyOverview.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="container mt-5">
+    <div id="propertyOverview" class="container mt-5 brand-ft-1">
         <div class="row">
             <div class="img-container col-12">
                 <img v-for="photo in property.photos" :key="photo.id" :src="'/storage/' + photo.file_name"
                     :alt="property.title + ' cover Photo'">
             </div>
         </div>
-        <div class="row mt-5">
+        <!-- <div class="row mt-5">
             <div class="property-details col-9">
                 <h2>
                     {{ property.title }}
@@ -17,7 +17,100 @@
                 <p class="mt-4">
                     {{ property.description }}
                 </p>
-            </div>
+            </div> -->
+
+
+            <nav id="propertyNav">
+                <div class="container-fluid">
+                    <ul class="row col-12 col-md-8 p-0">
+                        <li class="col-3 col-md-3 py-2 rounded mx-1 btn btn-outline-primary" 
+                            v-for="(infos, index) in propertyOverviewNav" :key="index"
+                            :class="{ 'bg-warning' : infos.isActive }"
+                            @click="$_activeNavOnClick(index)">
+
+                            {{ infos.name }}
+
+                        </li>
+                    </ul>
+
+                    <!-- Description section -->
+                    <div class="text-start" v-if="$_isActiveCheck(0)">
+
+                        <div class="row col-8">
+                            <h4 class="col-12 fw-bold">
+                                Dettagli dell'abitazione
+                            </h4>
+
+                            <div class="row col-12 col-md-5">
+                                <div class="col-6 fw-bold">
+                                    Numero bagni: 
+                                </div>
+
+                                <div class="col-6">
+                                    {{ property.bathroom_no }}
+                                </div>
+
+                                <div class="col-6 fw-bold">
+                                    Numero stanze:  
+                                </div>
+
+                                <div class="col-6">
+                                    {{ property.room_no }}
+                                </div>
+
+                            </div> 
+
+                            <div class="row col-12 col-md-5">
+
+                                <div class="col-6 fw-bold">
+                                    Numero letti:  
+                                </div>
+
+                                <div class="col-6">
+                                    {{ property.bed_no }}
+                                </div>
+
+                                <div class="col-6 fw-bold">
+                                    Metri Quadrati: 
+                                </div>
+
+                                <div class="col-6">
+                                    {{ property.square_meters }}
+                                </div>
+
+                            </div>
+                            
+                            <hr class="my-4">
+
+                            <div class="col-8">
+                                <h4 class="fw-bold">
+                                    Descrizione
+                                </h4>
+                                {{ property.description }}
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <!-- Services section -->
+                    <div class="text-start" v-if="$_isActiveCheck(1)">
+                        <h4 class="fw-bold">
+                            I servizi inclusi nell'abitazione:
+                        </h4>
+                        <div v-for="service in property.services" :key="service.id">
+                            {{ service.name }}
+                        </div>
+                    </div>
+
+                    <!-- Map section -->
+                    <div class="text-start" v-if="$_isActiveCheck(2)">
+                        {{ property.title }}
+                    </div>
+
+                </div>
+            </nav>
+
+
             <div class="col-3">
                 <h5 class="ms-2">Servizi:</h5>
                 <ul class="mt-1 list-group list-group-flush">
@@ -42,50 +135,100 @@
 <script>
 import axios from "axios";
 export default {
-    props: {
-        id: {
-            type: [String, Number],
-            required: true,
-        },
+  props: {
+    id: {
+      type: [String, Number],
+      required: true,
     },
-    data: function () {
-        return {
-            property: [],
-            user: "",
-        }
+  },
+  data: function () {
+    return {
+      property: [],
+      user: "",
+      show: false,
+      isActive: false,
+      currentActive: 0,
+
+      propertyOverviewNav: [
+        {
+          name: "Descrizione",
+          isActive: true,
+        },
+        {
+          name: "Servizi",
+          isActive: false,
+        },
+        {
+          name: "Mappa",
+          isActive: false,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    $_activeNavOnClick(index) {
+      this.propertyOverviewNav.forEach((item) => {
+        item.isActive = false;
+      });
+
+      this.currentActive = index;
+      this.propertyOverviewNav[index].isActive = true;
     },
 
-    methods: {
-        getPropertyData() {
-            axios.get("/api/apartments/" + this.id).
-                then(response => {
-                    this.property = response.data.results.data;
-                    this.user = this.property.user.surname + " " + this.property.user.name;
-                })
-                .catch(error => console.error(error.message))
-        }
+    $_isActiveCheck(index) {
+      if (this.currentActive === index) {
+        return true;
+      }
+      return false;
     },
-    created() {
-        this.getPropertyData();
-    }
-}
+    getPropertyData() {
+      axios
+        .get("/api/apartments/" + this.id)
+        .then((response) => {
+          this.property = response.data.results.data;
+          console.log(response.data.results.data);
+          this.user =
+            this.property.user.surname + " " + this.property.user.name;
+        })
+        .catch((error) => console.error(error.message));
+    },
+  },
+  created() {
+    this.getPropertyData();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-div.img-container {
-    width: 100%;
-    max-height: 40vh;
-    overflow: hidden;
+#propertyOverview {
+  #propertyNav {
+    margin: 3rem 0;
+    ul {
+      list-style: none;
 
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
+      li {
+        text-align: center;
+        display: inline-block;
+      }
     }
+  }
+}
+
+div.img-container {
+  width: 100%;
+  max-height: 40vh;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 }
 
 .icon {
-    width: 1.5rem;
-    filter: opacity(50%);
+  width: 1.5rem;
+  filter: opacity(50%);
 }
 </style>

6d2ba4844962afaa28f5a10a1f72841131b173ce
Rossiitaa <rosario.scuotto@outlook.it>
2022-10-25 17:43:45 +0200

    fix: :art: little fix back office nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                     <!-- Right Side Of Navbar -->
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
+                            <li class="nav-item ms-auto me-5">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))

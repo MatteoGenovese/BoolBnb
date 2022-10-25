ba7a037a8e21c6afe4cc7a1e632faa003ed8cdb3
Luca Spennato <luca.spennato@gmail.com>
2022-10-25 18:36:15 +0200

    feat: :sparkles: autowriting words

--- resources/js/components/Home-Components/Jumbotron.vue
+++ resources/js/components/Home-Components/Jumbotron.vue
@@ -3,7 +3,16 @@
         <div id="jumbotron" class="row my-3 position-relative">
 
             <div class="title-wrapper col-12 col-lg-6 py-5">
-                <h1 class="w-75">Scopri il posto in cui ti piacerà vivere</h1>
+                <h1 class="w-75">Scopri il posto in cui ti piacerà 
+                   <div class="d-sm-inline d-md-block d-xl-inline">
+                        <span class="type-effect brand-color-prime p-1">
+                            {{ printedWord }}
+                        </span>
+                   </div>
+                </h1>
+               
+               
+
                 <p>Cerca tra migliaia di inserzioni</p>
                 <SearchBar/>
             </div>
@@ -24,15 +33,60 @@ export default {
     },
     data: function () {
         return {
+            isTyping: false,
+            printedWord: '',
+            wordsArray: ['Viaggiare. ', 'Scoprire. ', 'Vivere. '],
+            isFull: false,
+            i: -1,
+            currentWord: 0,
+            reset: false,
         };
     },
 
     methods: {
-}
+        write(){
+            if(this.isFull === false){
+                this.i++;
+                this.printedWord += this.wordsArray[this.currentWord][this.i];
+
+                if(this.i === this.wordsArray[this.currentWord].length -1){
+                    this.isFull = true;
+                    this.i = -1;
+                    this.currentWord++;
+                }
+        }
+      },
+      deleteWord(){
+        setInterval(() => {
+            if(this.isFull === true){
+              this.printedWord = this.printedWord.slice(0,this.printedWord.length -1);
+              if(this.printedWord.length === 0){
+                this.isFull = false;
+              }
+            }
+            if(this.wordsArray.length === this.currentWord){
+              this.currentWord = 0;
+            }
+        }, 50);
+      },
+
+      startWrite(){
+        this.isTyping = setInterval(() => {
+          this.write();
+        }, 300);
+      },
+    },
+    created(){
+      this.startWrite();
+      this.deleteWord();
+    },
 };
 </script>
 
 <style lang='scss' scoped>
+
+    @import '../../../sass/partials/_brandVariables.scss';
+
     #jumbotron {
         background-repeat: no-repeat;
         background-position: right;
@@ -58,5 +112,41 @@ export default {
                 background-position: center;
             }
         }
+
+        /* DEMO-SPECIFIC STYLES */
+        .type-effect{
+            overflow: hidden; /* Ensures the content is not revealed until the animation */
+            border-right: 2px solid orange; /* The typwriter cursor */
+            margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+            animation: 
+            typing 3.5s steps(30, end),
+            blink .5s step-end infinite;
+            }
+            /* The typing effect */
+            @keyframes typing {
+            from { width: 0 }
+            to { width: 100% }
+            }
+            /* The typewriter cursor effect */
+            @keyframes blink {
+            from, to { border-color: transparent }
+            50% { border-color: $primary }
+            }
+
+        .typewriter{
+            &::after{
+            content: '\0020';
+            border-right: 4px solid;
+            border-radius: 30px;
+            animation: blink-caret 1.25s step-end infinite;
+            }
+        }
+
+        // /* The typewriter cursor effect */
+        @keyframes blink-caret {
+            from, to { border-color: transparent }
+            50% { border-color: blue; }
+        }
+
     }
 </style>

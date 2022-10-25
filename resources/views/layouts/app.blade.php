9dd75c9da4cb7b1e986befce3e1063b6e0c25742
Rossiitaa <rosario.scuotto@outlook.it>
2022-10-25 13:08:11 +0200

    fix nav (to finish)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,26 +24,16 @@
 
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    BoolBNB
-                </a>
-                @auth()
-                    <a class="navbar-item px-3" href="{{ route("user.apartments.index") }}">
-                        I tuoi annunci
+                <div class="collapse navbar-collapse d-flex list-unstyled justify-content-between" id="navbarSupportedContent">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        BoolBNB
                     </a>
-                @endauth
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
+                    @auth()
+                        <a class="navbar-item me-auto" href="{{ route("user.apartments.index") }}">
+                            I tuoi annunci
+                        </a>
+                    @endauth
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
@@ -55,8 +45,8 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item mx-5">
-                                <a href="{{ route('user.apartments.create') }}" class="btn btn-success m-auto">Crea</a>
+                            <li class="nav-item">
+                                <a href="{{ route('user.apartments.create') }}" class="btn btn-success me-3">Crea</a>
                             </li>
 
                                 <li class="nav-item dropdown">
@@ -76,9 +66,8 @@
                                         </form>
                                     </ul>
                                 </li>
-
+                                
                         @endguest
-                    </ul>
                 </div>
             </div>
         </nav>

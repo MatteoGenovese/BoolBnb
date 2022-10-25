e1d01a9f720f6a8fc97f89e324793a72f4b487e6
Luca Spennato <luca.spennato@gmail.com>
2022-10-25 15:32:38 +0200

    footerfix

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,90 +1,92 @@
 <template>
 <footer>
    <div class="container">
-      <div class="row justify-content-evenly">
-         <ul class="col-2">
+      <div class="footer-nav-links row py-5 justify-content-between align-items-center">
+         <ul class="col-12 col-lg-2 mb-3">
             <li>
                 <img :src="'/storage/asset/img/logo.jpg'" alt="brand logo">
             </li>
-            <li class="m-2 mt-0 over">
-                <a href="#"><span>Scampia</span></a>
+            <li>
+                Email
             </li>
-            <li class="m-2 over">
-                <a href="#"><span>+39 33333333333</span></a>
+            <li>
+               boolbnb@gmail.com
+            </li>
+            <li>
+                +39 33333333333
             </li>
          </ul>
 
-         <ul class="col-2 space-top ">
+         <ul class="col-6 col-sm-6 col-md-6 col-lg-2">
             <li class="title">
-                <span>Compagnia</span>
+                Compagnia
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Su di noi</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Lavoro</a> <a href="#" id="hire">Assumiamo</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Clienti</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Contattaci</a>
             </li>
          </ul>
 
-         <ul class="col-2 space-top">
+         <ul class="col-6 col-sm-6 col-md-6 col-lg-2">
             <li class="title">
                 <span>Caratteristiche</span>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Stampa</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Note di publicazione</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Integrazioni</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Prezzi</a>
             </li>
          </ul>
 
-         <ul class="col-2 space-top">
+         <ul class="col-6 col-sm-6 col-md-6 col-lg-2">
             <li class="title">
                 <span>Documentazione</span>
-            <li class="voice">
+            <li>
                 <a href="#" >Supporto</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Documenti</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Status</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >API</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Requisiti tecnici</a>
             </li>
          </ul>
 
-         <ul class="col-2 space-top">
+         <ul class="col-6 col-sm-6 col-md-6 col-lg-2">
             <li class="title">
                 <span>Risorse</span>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Aiuto</a>
             </li>
-            <li class="voice">
+            <li>
                 <a href="#" >Il tuo account</a>
             </li>
          </ul>
       </div>
       <div class="spacer d-flex"></div>
-      <div id="info">
-        <div class="d-flex justify-content-between">
+      <div class="row">
             <ul class="d-flex privacy-ul">
                <li><a href="#">Privacy & Policy</a></li>
                <li class="divider">/</li>
@@ -92,67 +94,28 @@
                <li class="divider">/</li>
                <li><a href="#">Mappa del sito</a></li>
             </ul>
-            <ul>
-                <li>Vito Farenga <a href="https://github.com/Vitofarenga">
-                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                     </svg>
-                     </a> 
-                     <a href="https://www.linkedin.com/in/vito-farenga-098bb7252/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-                     </svg>
-                     </a>
-                </li>
-                <li class="voice">Daniele Canale 
-                  <a href="https://github.com/DanCan30">
+        <div class="row justify-content-between justify-content-md-around">
+            <div class="col-6 col-md-4 col-lg-2 text-center text-white my-2" v-for="(dev, i) in devInfo" :key="i">
+ 
+                <a :href="dev.linkGit" class="m-1">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                        <path class="text-white" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                     </svg>
-                  </a>
-                  <a href="https://www.linkedin.com/in/daniele-canale/">
+                </a>
+
+                <a :href="dev.linkedin" class="m-1">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                        <path class="text-white" d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
                     </svg>
-                  </a>
-                </li>
-                <li class="voice">Matteo Genovese 
-                    <a href="">
-                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                       <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                       </svg>
-                    </a>
-                    <a href="https://www.linkedin.com/in/matteogenovese/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-                        </svg>
-                    </a>
-                </li>
-                <li class="voice">Luca Spennato 
-                    <a href="https://github.com/LucaSpennato">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                        </svg>
-                    </a>
-                    <a href="https://www.linkedin.com/in/lucaspennato/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-                        </svg>
-                    </a>
-                </li>
-                <li class="voice">Rosario Scuotto
-                    <a href="https://github.com/Rossiitaa">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                        </svg>
-                    </a>
-                    <a href="https://www.linkedin.com/in/scuotto-rosario/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-                        </svg>
-                    </a>
-                </li>
-            </ul>
+                </a>
+                
+                <h6>
+                    {{ dev.name }}
+                </h6>
+
+            </div>
         </div>
+
       </div>
    </div>
 </footer>
@@ -161,6 +124,38 @@
 
 <script >
 export default {
+    name: 'FooterComponent',
+    data(){
+        return{
+            devInfo:[
+                {
+                    name: 'Daniele Canale',
+                    linkGit: 'https://github.com/DanCan30',
+                    linkedin: 'https://www.linkedin.com/in/daniele-canale/',
+                },
+                {
+                    name: 'Rosario Scuotto',
+                    linkGit: 'https://github.com/Rossiitaa',
+                    linkedin: 'https://www.linkedin.com/in/scuotto-rosario/',
+                },
+                {
+                    name:'Vito Farenga',
+                    linkGit:'https://github.com/Vitofarenga',
+                    linkedin: 'https://www.linkedin.com/in/vito-farenga-098bb7252/',
+                },
+                {
+                    name:'Luca Spennato',
+                    linkGit:'https://github.com/LucaSpennato',
+                    linkedin: 'https://www.linkedin.com/in/lucaspennato/',
+                },
+                {
+                    name:'Matteo Genovese',
+                    linkGit:'https://github.com/MatteoGenovese',
+                    linkedin: 'https://www.linkedin.com/in/matteogenovese/',
+                },
+            ],
+        }
+    }
 
 }
 </script>
@@ -171,32 +166,37 @@ footer{
     background-color: $primary;
     width: 100%;
     padding-bottom: 2rem;
+    img{
+        width: 10rem;
+    }
+
+    ul{
+        list-style: none;
+        color: white;
+
+        li a{
+            color: white;
+            text-decoration: none;
+        }
+    }
+
+    .footer-nav-links{
+        ul{
+            padding-left: 1rem;
+            margin: auto;
+            margin-top: 1rem;
+
+            li{
+                margin-top: .5rem;
+            }
+        }
+    }
 }
-img{
-    width: 10rem;
-}
-ul{
-    list-style: none;
-    color: white;
-}
-i{
- 
- height: 5rem;
-}
-a{
-    color: white;
-    text-decoration: none;
-}
+
 .title{
     font-weight: bold;
     margin-bottom: .5rem;
     color: white;
-}
-.space-top{
-    margin-top: 3rem;
-}
-.voice{
-    margin-top: .5rem;
 }
 #hire{
     background-color: rgb(161, 59, 0);
@@ -221,16 +221,6 @@ a:hover{
     padding-right: 1rem;
     color: rgb(210, 209, 209);
 }
-.privacy-ul{
-    color: white;
-}
-.privacy-ul:hover{
-    color: rgb(210, 209, 209);
-}
-i{
-    color: white;
-    width: 1rem;
-    height: 1rem;
-}
+
 
 </style>
